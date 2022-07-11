471b19080072b0ddc3c6492ca497b926a6c7c91a
Sid <techiesid1985@gmail.com>
2022-07-11 21:13:20 +0530

    Code changes for custom 404 page

---
+++ plotly_deep_learning_app/pages/not_found_404.py
@@ -0,0 +1,11 @@
+from dash import html
+import dash
+import dash_bootstrap_components as dbc
+
+dash.register_page(__name__)
+
+layout =html.Div(children=[
+        dbc.Button("Go back to Home", size="lg", id="home_btn_404",href="/"),
+],className="bg")
+
+
